6437fbf698a49031ec43be0e37985c45f05f7151
chhilrim <chhilrim008@gmail.com>
2026-03-18 15:39:41 +0700

    feature/add-minus1

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,10 @@
-const sum = require('../src/sum');
+const {sum, minus }
+= require('../src/sum');
 
 test('adds 1 + 2 = 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('minus 5 - 3 = 2', () => {
+  expect(minus(5, 3)).toBe(2);
 });
